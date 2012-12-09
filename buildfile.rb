cf0b67efa4bf6c155dfcbb83e0430c76a778270a
nswarr <nswarr@glgroup.com>
2012-12-09 11:53:08 -0500

    More build script refactoring.

--- buildfile.rb
+++ buildfile.rb
@@ -28,6 +28,8 @@ SOLR = struct(
   :velocity     => solr_dependency('org.apache.solr:solr-velocity:jar:')
 )
 
+SOLR_HOME = 'ts-solr/solr-home/'
+
 desc "Build Solr code for People Inquiry"
 
 define "Pi-Solr" do
@@ -48,19 +50,22 @@ define "Pi-Solr" do
   # patches for heirarchical multi-faceting and multi-prefixed facets
   package(:jar, :file=>_('target/glgrecommend.jar')).include('com/**').exclude('org/**').enhance do
 
-    filter.from('lib').into('ts-solr/solr-home/lib').include('solr-mongo-importer-1.0.0.jar').run
-    compile.dependencies.map { |dep| FileUtils.cp dep.to_s , 'ts-solr/solr-home/lib' }
+    filter.from('lib').into("#{SOLR_HOME}lib").include('solr-mongo-importer-1.0.0.jar').run
+    compile.dependencies.map { |dep| FileUtils.cp dep.to_s , "#{SOLR_HOME}lib" }
 
-    solr_war_temp_location = 'ts-solr/solr-home/lib/temp/'
-    FileUtils.rm_rf solr_war_temp_location
-    FileUtils.mkdir solr_war_temp_location
+    solr_war_temp_location = 'target/solr/'
+    war_name = "solr-#{SOLR_VERSION}.war"
+    solr_war = "#{SOLR_HOME}lib/solr-#{SOLR_VERSION}.war"
 
-    solr_war_temp_name = 'solr-' + SOLR_VERSION + '.war'
-    FileUtils.mv 'ts-solr/solr-home/lib/' + solr_war_temp_name , solr_war_temp_location + solr_war_temp_name
-    sh "unzip " + solr_war_temp_location + solr_war_temp_name + " -d " + solr_war_temp_location
+    FileUtils.mv 'ts-solr/solr-home/lib/' + war_name , solr_war_temp_location + war_name
+    sh "unzip " + solr_war_temp_location + war_name + " -d " + solr_war_temp_location
     sh "cd target/classes; zip -r ../../ts-solr/solr-home/lib/temp/WEB-INF/lib/apache-solr-core-#{SOLR_VERSION}.jar org; cd ../.. "
     sh "cd ts-solr/solr-home/lib/temp; zip -r ../../solr.war *; cd ../../../.."
     FileUtils.rm_rf solr_war_temp_location
+  end
+
+  task :config_env do
+    File.open('/home/nswarr/data-config.xml', 'w'){|f| f.write(xml.to_xml)}
   end
 
 end
