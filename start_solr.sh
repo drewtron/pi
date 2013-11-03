c0c31e085be9c600a44881deafa3fb80418bfe26
Jeff O'Connell <joconnell@glgroup.com>
2013-11-03 18:28:08 +0000

    switch to indexes to be free of stops, stems, and highlighting

--- start_solr.sh
+++ start_solr.sh
@@ -1,4 +1,4 @@
 cd ts-solr/jetty-home
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=7666  -Xmx8096m -Dsolr.solr.home=../solr-home -Djava.util.logging.config.file=./etc/solr-logging.properties  -jar start.jar
+java -Xmx8096m -Dsolr.solr.home=../solr-home -Djava.util.logging.config.file=./etc/solr-logging.properties  -jar start.jar
 cd ../../
 
